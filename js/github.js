27f540e55740891ae9d38320c4d299f184e349e1
Huy Tu <tuhq@appstate.edu>
2016-10-19 19:37:19 -0400

    Finished the nocking stuffssssss

--- js/github.js
+++ js/github.js
@@ -37,8 +37,9 @@ function getRepos(userName)
 
 function getIssues(owner, repo)
 {
-	var url = "/repos/" + owner + "/" + repo + "/issues";
-	var mockService = nock("https://github.ncsu.edu/api/v3")
+
+	var url = "/api/v3/repos/" + owner + "/" + repo + "/issues?state=all";
+	var mockService = nock("https://github.ncsu.edu")
     .persist() // This will persist mock interception for lifetime of program.
     .get(url)
     .reply(200, JSON.stringify(data.issuesList) );
