9eaa89dd87e9078c4361ff6ee0609a2418db4bbf
Muhammad Ammar Ansari <ammaransari004@gmail.com>
2016-10-20 20:45:27 -0400

    Removed dead code

--- js/main.js
+++ js/main.js
@@ -6,21 +6,7 @@ var _ = require("underscore");
 var stringSimilarity = require('string-similarity');
 var github = require("./github.js");
 
-// Which person is assigned to most to issues?
-function findMostFrequentAssignee(user,repo)
-{
-	return new Promise(function (resolve, reject)
-	{
-		// mock data needs list of issues.
-		github.getIssues(user,repo).then(function (issues)
-		{
-			var names = _.pluck(issues,"assignee")
-			var frequency = _.countBy(names, function (name) { return name; });
-			var max = _.max(_.keys(frequency), function(item){ return frequency[item] })
-			resolve({userName: max, count: frequency[max]});
-		});
-	});
-}
+
 
 // Return open issues in a user's repo
 function countOpen(user,repo)
@@ -144,24 +130,6 @@ function getFreeDevelopers(owner,repo, number)
 					return false;
 				}
 			});
-			/*
-			if(!similarIssues.length){
-				reject("Sorry, couldn't find anyone to help you");
-			}
-			
-			var result =[];
-			//TODO Strip date
-			for(i=0;i < similarIssues.length;i++){
-				for(j = 0; j<similarIssues[i].assignees.length;j++)
-				{
-					result.push(similarIssues[i].assignees[j].login);
-				}
-			}
-			yissuedl = [];
-			for(i=0;i < yissue.assignees.length;i++){
-				yissuedl.push(yissue.assignees[i].login);
-			}
-			result = _.difference(result, yissuedl);*/
 			var result =[];
 			yissuedl = [];
 
@@ -187,68 +155,7 @@ function getFreeDevelopers(owner,repo, number)
 });
 }
 
-
-
-/*function getDeadlinesForUser(owner,repo,assigneeName)
-{
-	return new Promise(function (resolve, reject)
-	{
-		// mock data needs list of issues.
-		getIssuesAssigedToAuser(user,repo,assigneeName).then(function (issues)
-		{
-			var result=[];
-			for( var i = 0; i < issues.length; i++ )
-			{
-				getAMileStone(owner, repo, issue.milestone.number).then(function(milestone){
-
-				});
-
-			}
-			/*var issuesWithMileStones = _.reject(issues,function(issueVar){ return issueVar.milestone == null; });
-
-			for( var i = 0; i < issuesWithMileStones.length; i++ )
-			{
-				var name = issuesWithMileStones[i].milestone.number;
-				resSet.push(name);
-			}
-			var resSet=[];
-			//extracting issues with milestones
-			var issuesWithAssignee =  _.reject(issues,function(issueVar){ return issueVar.assignee == null; });
-
-
-
-
-			resolve(result);
-		});
-	});
-}
-*/
-
-// How many words in an issue's title version an issue's body?
-function titleBodyWordCountRatio(user,repo,number)
-{
-	return new Promise(function (resolve, reject)
-	{
-		// mock data needs list of issues.
-		github.getAnIssue(user,repo,number).then(function (issue)
-		{
-			var titleWords = issue.title.split(/\W+|\d+/).length;
-			var bodyWords  = issue.body.split(/\W+|\d+/).length;
-			if( issue.body == "" )
-			{
-				resolve("NA");
-				// HINT: http://stackoverflow.com/questions/4964484/why-does-split-on-an-empty-string-return-a-non-empty-array
-			}
-			//console.log( titleWords, bodyWords, issue.body);
-			var str = ( titleWords / bodyWords ) + "";
-			resolve(str);
-		});
-	});
-}
-
 exports.getIssuesAssigedToAuser = getIssuesAssigedToAuser;
-exports.findMostFrequentAssignee = findMostFrequentAssignee;
 exports.assignIssueToUser = assignIssueToUser;
 exports.countOpen = countOpen;
 exports.getFreeDevelopers=getFreeDevelopers;
-exports.titleBodyWordCountRatio = titleBodyWordCountRatio;
